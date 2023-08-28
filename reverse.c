3e3c38f8456f4e8cbc3d5187f33775f2ea5edbf6
kc01-8 <minecraftandcallofduty101@gmail.com>
2023-08-28 23:06:42 +0300

    Create reverse.c

---
+++ reverse.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+void reverse(int array[], int length);
+
+int i; 
+
+int main(void){
+    int array[] = {1,2,3,4,5,6,7,8,9};
+    int temp = 0;
+    int size = sizeof(array) / sizeof(array[0]);
+
+    reverse(array, size);
+
+    for (i = 0; i < 9; i++){
+        printf("%d\n", array[i]);
+    }
+
+    printf("\n\n");
+}
+
+void reverse(int array[], int size){
+    int temp = 0;
+
+    for (i = 0; i < (size / 2); i++){
+        temp = array[i];
+        array[i] = array[size - i - 1];
+        array[size - i - 1] = temp;
+    }
+}
